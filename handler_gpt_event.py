8d81000c92147bbdee5e6aa19596a49e645ad1a9
pintok <pintok@machine.home>
2025-03-23 00:42:21 +0100

    web, image, text et reasonning s'affiche correctement en delta. Pour les function, ne pas faire de stream mais une autre route, en, faire aussi une pour le computer use, le structured output se fera sur le stream (si implémenté), le file_search sur le stream également. Il faut également passer les params dans le body

---
+++ handler_gpt_event.py
@@ -0,0 +1,30 @@
+from openai.types.responses import *
+
+def serialize(obj):
+    if hasattr(obj, "__dict__"):
+        return {key: serialize(value) for key, value in obj.__dict__.items()}
+    elif isinstance(obj, list):
+        return [serialize(item) for item in obj]
+    elif isinstance(obj, dict):
+        return {key: serialize(value) for key, value in obj.items()}
+    else:
+        return obj
+
+def handle_event(event):
+    # print(type(event).__name__)
+    if isinstance(event, ResponseTextDeltaEvent):
+        print(event.delta)
+        yield {'type': 'delta', 'content': event.delta, 'allEvent': serialize(event)}
+    # elif isinstance(event, ResponseCompletedEvent):
+    #     output = ""
+    #     for output in event.response.output:
+    #         if "content" in output:
+    #             output = output
+    #             break
+    #     print(output.content[0].text)
+    #     yield {'type': 'complete', 'content': output.content[0].text, 'allEvent': serialize(event)}
+    #     pass
+    else:
+        # yield {'type': 'unhandled', 'content': type(event).__name__, 'allEvent': serialize(event)}
+        yield {'type': 'unhandled', 'content': "", 'allEvent': serialize(event)}
+        pass
--- handler_openai.py
+++ handler_openai.py
@@ -1,48 +1,60 @@
 import json
+import os
+import types
 
-from flask import Flask, Response, jsonify, stream_with_context
+import openai
+from flask import Flask, request, jsonify, Response
 from openai import OpenAI
-from openai.types.responses import *
+from handler_gpt_event import handle_event
+from time import time
 
 app = Flask(__name__)
+app.app_context().push()
 
-def stream_response():
-    client = OpenAI(api_key="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")
-    stream = client.responses.create(
-        model="gpt-4o",
-        input=[
-            {
-                "role": "user",
-                "content": "Say 'double bubble bath' ten times fast.",
-            },
-        ],
-        stream=True,
-    )
+def stream_response(headers, params):
 
-    for event in stream:
-        # print(f"Received event: {event}")  # Ajoute ceci pour voir les événements
+    debut = time()
+    client = get_client_openai(headers)
 
-        if isinstance(event, ResponseTextDeltaEvent):
-            yield {'type': 'delta', 'content': event.delta}
-        if isinstance(event, ResponseCreatedEvent):
-            # yield {'type': 'event', 'name': 'Response Created'}
-            pass
-        elif isinstance(event, ResponseCompletedEvent):
-            # yield {'type': 'event', 'name': 'Response Completed'}
-            pass
-        # Ajoute d'autres elif pour les événements que tu veux gérer
-        else:
-            # yield {'type': 'unhandled', 'name': type(event).__name__}
-            pass
+    try:
+        stream = client.responses.create(
+            model="gpt-4o",
+            input=[
+                {
+                    "role": "user",
+                    "content": "quel est la météo de demain ? J'habite à Alfortville",
+                },
+            ],
+            stream=True,
+        )
+        for event in stream:
+            yield handle_event(event)
+    except openai.AuthenticationError:
+        yield {"ERREUR":"La cle API n'est pas bonne ou inexistante. Il faut la passer (par ordre de priorite) soit dans le Authorization Header ou la mettre dans une variable d'environnement tokenGPT ou OPENAI_API_KEY"}
+    # print(f"Stream de la réponse total en {round(time()-debut, 2)}secs")
 
-@app.route('/stream')
+@app.route('/stream', methods=["POST"])
 def stream():
-    # print(stream_response())
-    # def generate():
-    for data in stream_response():
-        # print(data)
-        yield json.dumps(data) + "\n"
-    # return Response(generate())
+    body = request.json
+    headers = request.headers
+    def generate():
+        response = {"error":"Pas un generateur"}
+        for data in stream_response(headers, body):
+            if isinstance(data, types.GeneratorType):
+                response = list(data)[0]
+            elif isinstance(data, dict): response = data
+            yield response["content"]
+        yield "\n"
+    return Response(generate(), content_type='application/json')
+
+def get_client_openai(headers):
+    try: return OpenAI(api_key=headers.get('Authorization'))
+    except:
+        try:
+            return OpenAI(api_key=os.environ.get("tokenGPT"))
+        except:
+            return OpenAI()
+    return 0
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000)
